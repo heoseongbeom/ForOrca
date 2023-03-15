29281af3ba32e7cb576dda923191c5f634456459
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-03-15 12:33:14 -0700

    Create AboutController.java
    About 컨트롤러 생성

--- src/main/java/com/orca/kim/about/controller/AboutController.java
+++ src/main/java/com/orca/kim/about/controller/AboutController.java
@@ -1,10 +1,16 @@
 package com.orca.kim.about.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.orca.kim.about.model.service.AboutService;
+
 @Controller
 public class AboutController {
+	
+	@Autowired
+	private AboutService aService;
 	
 	@RequestMapping("about.me")
 	public String aboutPage() {
