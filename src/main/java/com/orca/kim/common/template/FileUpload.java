cf4fea03450bcd4ac3dae3ed71c836c83c509eea
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-03-15 12:22:39 -0700

    Update FileUpload.java

--- src/main/java/com/orca/kim/common/template/FileUpload.java
+++ src/main/java/com/orca/kim/common/template/FileUpload.java
@@ -14,24 +14,25 @@ public class FileUpload {
 	public static String saveFile(MultipartFile upfile, HttpSession session, String folderPath) {
 			
 			// 파일명 수정 작업 후 서버에 업로드 
-			String originName = upfile.getOriginalFilename(); // "flower.png"
+			String mainPhotoOrigin = upfile.getOriginalFilename(); // "flower.png"
 			
 			String currentTime = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
 			int ranNum = (int)(Math.random() * 90000 + 10000);
-			String ext = originName.substring(originName.lastIndexOf("."));
+			String ext = mainPhotoOrigin.substring(mainPhotoOrigin.lastIndexOf("."));
 			
-			String changeName = currentTime + ranNum + ext;
+			String mainPhotoChange = currentTime + ranNum + ext;
 			
 			// 업로드 시키고자하는 폴더의 물리적인 경로 알아내기
 			String savePath = session.getServletContext().getRealPath(folderPath);
 			
 			try {
-				upfile.transferTo(new File(savePath + changeName));
+				upfile.transferTo(new File(savePath + mainPhotoChange));
 			} catch (IllegalStateException | IOException e) {
 				e.printStackTrace();
 			}
 				// resources/xxxx/
-			return folderPath + changeName;
+			return folderPath + mainPhotoChange;
 		}
 
+	
 }
