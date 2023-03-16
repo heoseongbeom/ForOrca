47f060d1a11a44c09707d08e95a73b5ae680e28d
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-03-16 02:13:57 -0700

    Update AboutDao.java
    about, aboutUpdate 페이지 select 구문

--- src/main/java/com/orca/kim/about/model/dao/AboutDao.java
+++ src/main/java/com/orca/kim/about/model/dao/AboutDao.java
@@ -1,8 +1,16 @@
 package com.orca.kim.about.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.orca.kim.about.model.vo.About;
 
 @Repository
 public class AboutDao {
+	
+	// about, aboutUpdate 페이지 select 구문
+	public About selectAbout(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("aboutMapper.selectAbout");
+	}
 
 }
