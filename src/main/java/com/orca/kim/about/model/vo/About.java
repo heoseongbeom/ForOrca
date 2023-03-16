9f4b43a640b255906cf81c6712958b1486b97f15
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-03-15 18:06:46 -0700

    Update About.java
    About vo객체 생성

--- src/main/java/com/orca/kim/about/model/vo/About.java
+++ src/main/java/com/orca/kim/about/model/vo/About.java
@@ -1,5 +1,74 @@
 package com.orca.kim.about.model.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
 public class About {
+	
+	private int aboutNo;
+	private String aboutIntroOne;
+	private String aboutTattooist;
+	private String aboutBirthday;
+	private String aboutWebsite;
+	private String aboutPhone;
+	private String aboutCity;
+	private String aboutAge;
+	private String aboutDegree;
+	private String aboutEmail;
+	private String aboutFreelance;
+	private String aboutIntroTwo;
+	private String aboutSkills;
+	private String aboutSkillsOne;
+	private String aboutSkillsOnePer;
+	private String aboutSkillsTwo;
+	private String aboutSkillsTwoPer;
+	private String aboutSkillsThree;
+	private String aboutSkillsThreePer;
+	private String aboutSkillsFour;
+	private String aboutSkillsFourPer;
+	private String aboutSkillsFive;
+	private String aboutSkillsFivePer;
+	private String aboutSkillsSix;
+	private String aboutSkillsSixPer;
+	private String aboutFacts;
+	private String aboutFactsOne;
+	private String aboutFactsOneText;
+	private String aboutFactsTwo;
+	private String aboutFactsTwoText;
+	private String aboutFactsThree;
+	private String aboutFactsThreeText;
+	private String aboutFactsFour;
+	private String aboutFactsFourText;
+	private String aboutStory;
+	private String aboutStOneText1;
+	private String aboutStOneText2;
+	private String aboutStOneText3;
+	private String aboutStTwoText1;
+	private String aboutStTwoText2;
+	private String aboutStTwoText3;
+	private String aboutStThreeText1;
+	private String aboutStThreeText2;
+	private String aboutStThreeText3;
+	private String aboutStFourText1;
+	private String aboutStFourText2;
+	private String aboutStFourText3;
+	private String aboutStFiveText1;
+	private String aboutStFiveText2;
+	private String aboutStFiveText3;
+	private String aboutProfileImg;
+	private String aboutStoryImg1;
+	private String aboutStoryImg2;
+	private String aboutStoryImg3;
+	private String aboutStoryImg4;
+	private String aboutStoryImg5;
+	
 
 }
