621bda00fab82fb745f86af664a2bb933cd6a099
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-03-16 02:14:02 -0700

    Update AboutService.java
    about, aboutUpdate 페이지 select 구문

--- src/main/java/com/orca/kim/about/model/service/AboutService.java
+++ src/main/java/com/orca/kim/about/model/service/AboutService.java
@@ -1,5 +1,10 @@
 package com.orca.kim.about.model.service;
 
+import com.orca.kim.about.model.vo.About;
+
 public interface AboutService {
+	
+	// about, aboutUpdate 페이지 select 구문
+	About selectAbout();
 
 }
