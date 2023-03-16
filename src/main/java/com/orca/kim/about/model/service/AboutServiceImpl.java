9310fc3aa0ba9a58b6c91f444a65a160348d5981
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-03-16 02:14:05 -0700

    Update AboutServiceImpl.java
    about, aboutUpdate 페이지 select 구문

--- src/main/java/com/orca/kim/about/model/service/AboutServiceImpl.java
+++ src/main/java/com/orca/kim/about/model/service/AboutServiceImpl.java
@@ -1,8 +1,25 @@
 package com.orca.kim.about.model.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.orca.kim.about.model.dao.AboutDao;
+import com.orca.kim.about.model.vo.About;
 
 @Service
 public class AboutServiceImpl implements AboutService{
+	
+	@Autowired
+	private AboutDao aDao;
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	// about, aboutUpdate 페이지 select 구문
+	@Override
+	public About selectAbout() {
+		return aDao.selectAbout(sqlSession);
+	}
 
 }
