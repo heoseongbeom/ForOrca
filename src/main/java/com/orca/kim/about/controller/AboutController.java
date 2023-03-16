f6182387af889531254617406af91c309a645951
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-03-16 02:13:21 -0700

    Update AboutController.java
    about.me : about 페이지 이동 및 select 컨트롤러
    aboutUpdate : aboutUpdate 페이지 이동 및 select 컨트롤러

--- src/main/java/com/orca/kim/about/controller/AboutController.java
+++ src/main/java/com/orca/kim/about/controller/AboutController.java
@@ -1,10 +1,13 @@
 package com.orca.kim.about.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.orca.kim.about.model.service.AboutService;
+import com.orca.kim.about.model.vo.About;
 
 @Controller
 public class AboutController {
@@ -12,8 +15,23 @@ public class AboutController {
 	@Autowired
 	private AboutService aService;
 	
+	// about 패이지 이동 select 컨트롤러
 	@RequestMapping("about.me")
-	public String aboutPage() {
-		return "about/about";
+	public ModelAndView selectAbout(ModelAndView mv) {
+		
+		About a = aService.selectAbout();
+		System.out.println(a);
+		mv.addObject("a", a).setViewName("about/about");
+		return mv;
 	}
+	
+	// aboutUpdate 페이지 이동 및 select 컨트롤러
+	@RequestMapping("aboutUpdateForm.me")
+	public ModelAndView aboutUpdateForm(ModelAndView mv) {
+		About a = aService.selectAbout();
+		System.out.println(a);
+		mv.addObject("a", a).setViewName("about/aboutUpdate");
+		return mv;
+	}
+	
 }
