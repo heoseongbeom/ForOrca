1224c61835a0203d64b33218eb182e6176cb25de
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-03-27 01:09:09 -0700

    Update MemberService.java
    confirmEmail (이메일 중복 확인)
    
    selectEmail (이메일 찾기)
    
    selectPwd (비밀번호 찾기)       추가

--- src/main/java/com/orca/kim/member/model/service/MemberService.java
+++ src/main/java/com/orca/kim/member/model/service/MemberService.java
@@ -11,12 +11,19 @@ public interface MemberService {
 	// 회원가입 기능
 	int signupMember(Member m);
 	
+	// 이메일 중복 확인
+	int confirmEmail(String email);
+	
+	// 이메일 찾기
+	Member selectEmail(Member m);
+	
+	// 비밀번호 찾기
+	Member selectPwd(Member m);
+	
 	// 메인 페이지 select
 	MainPage selectMain();
 	
 	// 메인 페이지 update
 	int updateMain(MainPage mp);
 	
-	// 이메일 중복 확인
-	int confirmEmail(String email);
 }
