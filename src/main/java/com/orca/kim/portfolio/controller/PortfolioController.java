9034e528236e71d71fddeb27e2517f0f091d0f5e
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-04-01 23:49:45 -0700

    Update PortfolioController.java
    portfolioAdd(업로드) 페이지 이동 컨트롤러 추가

--- src/main/java/com/orca/kim/portfolio/controller/PortfolioController.java
+++ src/main/java/com/orca/kim/portfolio/controller/PortfolioController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PortfolioController {
 	
+	// portfolio 페이지 이동 컨트롤러
 	@RequestMapping("portfolio.me")
 	public String portfolioPage() {
 		return "portfolio/portfolio";
+	}
+	
+	// portfolioAdd(업로드) 페이지 이동 컨트롤러
+	@RequestMapping("portfolioAdd.po")
+	public String portfolioAddPage() {
+		return "portfolio/portfolioAdd";
 	}
 	
 }
