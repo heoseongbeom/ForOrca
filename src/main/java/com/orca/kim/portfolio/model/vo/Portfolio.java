5f4b27273b5b48b5f7293ed46cf7ff03b83ae74a
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-04-02 23:13:01 -0700

    Create Portfolio.java
    Portfolio vo 객체 생성

---
+++ src/main/java/com/orca/kim/portfolio/model/vo/Portfolio.java
@@ -0,0 +1,23 @@
+package com.orca.kim.portfolio.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Portfolio {
+	
+	private String portNo;
+	private String portText;
+	private String portOriginPhoto;
+	private String portChangePhoto;
+	private String portCategory;
+	
+
+}
