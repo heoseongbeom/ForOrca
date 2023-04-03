de0bb8d44a78952fd28b6efc0465c7caab64ad7b
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-04-02 23:11:09 -0700

    Update PortfolioController.java
    portfolioAdd insert 컨트롤러 추가

--- src/main/java/com/orca/kim/portfolio/controller/PortfolioController.java
+++ src/main/java/com/orca/kim/portfolio/controller/PortfolioController.java
@@ -1,13 +1,24 @@
 package com.orca.kim.portfolio.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.orca.kim.common.template.FileUpload;
+import com.orca.kim.portfolio.model.service.PortfolioService;
+import com.orca.kim.portfolio.model.vo.Portfolio;
 
 @Controller
 public class PortfolioController {
 	
+	@Autowired
+	private PortfolioService pService;
+	
 	// portfolio 페이지 이동 컨트롤러
-	@RequestMapping("portfolio.me")
+	@RequestMapping("portfolio.po")
 	public String portfolioPage() {
 		return "portfolio/portfolio";
 	}
@@ -17,5 +28,31 @@ public class PortfolioController {
 	public String portfolioAddPage() {
 		return "portfolio/portfolioAdd";
 	}
+	
+	// portfolioAdd upload 컨트롤러
+	@RequestMapping("portfolioInsert.po")
+	public String insertPortfolio(Portfolio p, HttpSession session, MultipartFile upfile) {
+		System.out.println(p);
+		System.out.println(upfile);
+		
+		if(!upfile.getOriginalFilename().equals("")) {
+			String saveFilePath = FileUpload.saveFile(upfile, session, "resources/uploadFiles/");
+			p.setPortOriginPhoto(upfile.getOriginalFilename());
+			p.setPortChangePhoto(saveFilePath);
+		}
+		System.out.println(p);
+		int result = pService.insertPortfolio(p);
+		
+		if(result > 0) {
+			session.setAttribute("alertTitle", "업로드 완료");
+			session.setAttribute("alertMsg", "Upload Complete");
+			return "redirect:portfolio.po";
+		}else {
+			session.setAttribute("aloertTitle", "업로드 실패");
+			session.setAttribute("alertMsg", "Upload Complete");
+			return "redirect/";
+		}
+	}
+	
 	
 }
