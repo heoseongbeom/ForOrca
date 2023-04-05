fe44bc079e86d11e2cf55fdf9567934693cd34e9
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-04-05 00:36:05 -0700

    Update PortfolioService.java
    portfolio 페이지 select 기능
    portfolioAdd 페이지 insert 기능
    portfolioUpdateForm 페이지 select 기능
    추가

--- src/main/java/com/orca/kim/portfolio/model/service/PortfolioService.java
+++ src/main/java/com/orca/kim/portfolio/model/service/PortfolioService.java
@@ -1,9 +1,18 @@
 package com.orca.kim.portfolio.model.service;
 
+import java.util.ArrayList;
+
 import com.orca.kim.portfolio.model.vo.Portfolio;
 
 public interface PortfolioService {
+
+	// portfolio 페이지 select 기능
+	ArrayList<Portfolio> selectPortfolio();
 	
+	// portfolioAdd 페이지 insert 기능
 	int insertPortfolio(Portfolio p);
+	
+	// portfolioUpdateForm 페이지 select 기능
+	Portfolio selectPortfolioUpdateForm(int portNo );
 
 }
