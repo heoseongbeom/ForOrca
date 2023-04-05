07691958e3d8615f55aa31f44b9ed45704de8cf0
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-04-05 00:35:25 -0700

    Update PortfolioController.java
    portfolio 페이지 이동 컨트롤러
    portfolio 페이지 select 컨트롤러
    portfolio updateForm 페이지 이동 컨트롤러
    추가

--- src/main/java/com/orca/kim/portfolio/controller/PortfolioController.java
+++ src/main/java/com/orca/kim/portfolio/controller/PortfolioController.java
@@ -1,11 +1,14 @@
 package com.orca.kim.portfolio.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.orca.kim.common.template.FileUpload;
 import com.orca.kim.portfolio.model.service.PortfolioService;
@@ -18,9 +21,18 @@ public class PortfolioController {
 	private PortfolioService pService;
 	
 	// portfolio 페이지 이동 컨트롤러
+//	@RequestMapping("portfolio.po")
+//	public String portfolioPage() {
+//		return "portfolio/portfolio";
+//	}
+	
+	// portfolio 페이지 select 컨트롤러
 	@RequestMapping("portfolio.po")
-	public String portfolioPage() {
-		return "portfolio/portfolio";
+	public ModelAndView selectPortfolio(ModelAndView mv) {
+		ArrayList<Portfolio> list = pService.selectPortfolio();
+		mv.addObject("list", list).setViewName("portfolio/portfolio");
+		System.out.println(list);
+		return mv;
 	}
 	
 	// portfolioAdd(업로드) 페이지 이동 컨트롤러
@@ -32,15 +44,11 @@ public class PortfolioController {
 	// portfolioAdd upload 컨트롤러
 	@RequestMapping("portfolioInsert.po")
 	public String insertPortfolio(Portfolio p, HttpSession session, MultipartFile upfile) {
-		System.out.println(p);
-		System.out.println(upfile);
-		
 		if(!upfile.getOriginalFilename().equals("")) {
 			String saveFilePath = FileUpload.saveFile(upfile, session, "resources/uploadFiles/");
 			p.setPortOriginPhoto(upfile.getOriginalFilename());
 			p.setPortChangePhoto(saveFilePath);
 		}
-		System.out.println(p);
 		int result = pService.insertPortfolio(p);
 		
 		if(result > 0) {
@@ -52,6 +60,20 @@ public class PortfolioController {
 			session.setAttribute("alertMsg", "Upload Complete");
 			return "redirect/";
 		}
+	}
+	
+	// portfolio updateForm 페이지 이동 컨트롤러
+	@RequestMapping("portfolioUpdateForm.po")
+	public ModelAndView updateFormPortfolio(int no, ModelAndView mv, HttpSession session) {
+		System.out.println(no);
+		Portfolio p = pService.selectPortfolioUpdateForm(no);
+		System.out.println(p);
+		if(p != null) {
+			mv.addObject("p", p).setViewName("portfolio/portfolioUpdateForm");
+		}else {
+			mv.addObject("errorMsg", "상세조회 실패").setViewName("common/errorPage");
+		}
+		return mv;
 	}
 	
 	
