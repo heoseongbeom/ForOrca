45f179824e054d56abc0349ab256a9f0e836dac0
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-04-05 00:35:42 -0700

    Update PortfolioDao.java
    portfolio select 구문
    portfolioUpadteForm select 구문
    추가

--- src/main/java/com/orca/kim/portfolio/model/dao/PortfolioDao.java
+++ src/main/java/com/orca/kim/portfolio/model/dao/PortfolioDao.java
@@ -1,5 +1,7 @@
 package com.orca.kim.portfolio.model.dao;
 
+import java.util.ArrayList;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -8,9 +10,21 @@ import com.orca.kim.portfolio.model.vo.Portfolio;
 @Repository
 public class PortfolioDao {
 	
+	// portfolio select 구문
+	public ArrayList<Portfolio> selectPortfolio(SqlSessionTemplate sqlSession) {
+		return (ArrayList)sqlSession.selectList("portfolioMapper.selectPortfolio");
+	}
+	
 	// portfolioAdd insert 구문
 	public int insertPortfolio(Portfolio p, SqlSessionTemplate sqlSession) {
 		return sqlSession.insert("portfolioMapper.insertPortfolio", p);
 	}
+	
+	// portfolioUpadteForm select 구문
+	public Portfolio selectPortfolioUpdateForm(int portNo, SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("portfolioMapper.selectPortfolioUpdateForm", portNo);
+	}
+	
+	
 
 }
