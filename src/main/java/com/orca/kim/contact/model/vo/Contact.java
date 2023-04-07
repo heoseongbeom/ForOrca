02fd037876026f4138d6d8c9f28ee777f080b908
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-04-07 00:55:33 -0700

    Create Contact.java
    Contact vo 객체 생성

---
+++ src/main/java/com/orca/kim/contact/model/vo/Contact.java
@@ -0,0 +1,20 @@
+package com.orca.kim.contact.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Contact {
+	
+	private String conLocation;
+	private String conEmail;
+	private String conPhone;
+
+}
