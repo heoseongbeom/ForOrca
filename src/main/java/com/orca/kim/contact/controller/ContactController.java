2beed8ea67994e9b7ac83075f1ef685a52f51460
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-04-07 00:54:29 -0700

    Update ContactController.java
    contact 페이지 select 컨트롤러 추가

--- src/main/java/com/orca/kim/contact/controller/ContactController.java
+++ src/main/java/com/orca/kim/contact/controller/ContactController.java
@@ -1,14 +1,30 @@
 package com.orca.kim.contact.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.orca.kim.contact.model.service.ContactService;
+import com.orca.kim.contact.model.vo.Contact;
 
 @Controller
 public class ContactController {
 	
-	@RequestMapping("contact.me")
-	public String contactPage() {
-		return "contact/contact";
-				
+	@Autowired
+	private ContactService cService;
+	
+	// contact 페이지 select 컨트롤러
+	@RequestMapping("contact.co")
+	public ModelAndView selectContact(ModelAndView mv) {
+		
+		Contact c = cService.selectContact();
+		
+		if(c != null) {
+			mv.addObject("c",c).setViewName("contact/contact");
+		}else {
+			mv.addObject("errorMsg", "상세조회 실패").setViewName("common/errorPage");
+		}
+		return mv;
 	}
 }
