c00e8ac7cf16749fb61f813c039ce5571a1b42d3
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-04-07 21:50:09 -0700

    Update ContactService.java
    contactUpdate 페이지 update 기능
    
    추가

--- src/main/java/com/orca/kim/contact/model/service/ContactService.java
+++ src/main/java/com/orca/kim/contact/model/service/ContactService.java
@@ -4,6 +4,10 @@ import com.orca.kim.contact.model.vo.Contact;
 
 public interface ContactService {
 
+	// contact 페이지 select 기능
 	Contact selectContact();
+	
+	// contactUpdate 페이지 update 기능
+	int updateContact(Contact c);
 	
 }
