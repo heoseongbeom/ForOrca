fecc4bbd7bef2e0a5ae09713af53f49d8787d948
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-04-07 21:49:53 -0700

    Update ContactController.java
    contactUpdate 페이지 select 컨트롤러
    contactUpdate 페이지 update 컨트롤러
    
    추가

--- src/main/java/com/orca/kim/contact/controller/ContactController.java
+++ src/main/java/com/orca/kim/contact/controller/ContactController.java
@@ -1,5 +1,7 @@
 package com.orca.kim.contact.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +25,44 @@ public class ContactController {
 		if(c != null) {
 			mv.addObject("c",c).setViewName("contact/contact");
 		}else {
-			mv.addObject("errorMsg", "상세조회 실패").setViewName("common/errorPage");
+			mv.addObject("errorMsg", "페이지 조회 실패").setViewName("common/errorPage");
 		}
 		return mv;
 	}
+	
+	// contactUpdate 페이지 select 컨트롤러
+	@RequestMapping("contactUpdateForm.co")
+	public ModelAndView selectContactUpdateForm(ModelAndView mv) {
+			
+		Contact c = cService.selectContact();
+		
+		if(c != null) {
+			mv.addObject("c",c).setViewName("contact/contactUpdate");
+		}else {
+			mv.addObject("errorMsg", "페이지 조회 실패").setViewName("common/errorPage");
+			}
+			return mv;
+	} 
+	
+	// contactUpdate 페이지 update 컨트롤러
+	@RequestMapping("contactUpdate.co")
+	public String updateContact(Contact c, HttpSession session) {
+		
+		int result = cService.updateContact(c);
+		
+		if(result > 0) {
+			session.setAttribute("alertTitle", "수정 완료");
+			session.setAttribute("alertMsg", "Edit Complete");
+		}else {
+			session.setAttribute("alertTitle", "수정 살퍄");
+			session.setAttribute("alertMsg", "Edit Fail");
+		}
+		return "redirect:contact.co";
+	}
+	
+	
+	
+	
+	
+	
 }
