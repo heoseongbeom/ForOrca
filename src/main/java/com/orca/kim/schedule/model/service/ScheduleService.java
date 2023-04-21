7f9ea23d5f0e4a05cdf4453ca9226ed262adcc0f
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-04-20 17:35:04 -0700

    Update ScheduleService.java
    schedule 페이지 eventDrop ajax
    schedule 페이지 fullcalendar detail 수정
    
    추가

--- src/main/java/com/orca/kim/schedule/model/service/ScheduleService.java
+++ src/main/java/com/orca/kim/schedule/model/service/ScheduleService.java
@@ -8,4 +8,10 @@ public interface ScheduleService {
 
 	// schedule 페이지 fullcaledar 일정 조회  
 	ArrayList<Schedule> ajaxSelectScheduleList();
+	
+	// schedule 페이지 dropEvent ajax
+	int scheduleDropAjax(Schedule s);
+	
+	// schedule 페이지 fullcalendar detail 수정
+	int updateScheduleDetail(Schedule s);
 }
