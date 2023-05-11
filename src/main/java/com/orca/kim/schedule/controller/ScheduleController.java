090e4bbd0e7dc0acd02e2fff1cdf4cdf875e81fa
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-05-11 15:28:27 -0700

    Update ScheduleController.java
    관리자가 아닌 일반 회원이 schedule페이지 접속 시 scheduleClient페이지로 이동 하는 컨트롤러 추가
    
    scheduleClient 페이지 fullcalendar 일정 조회 ajax 컨트롤러 추가
    
    schedule 페이지 delete 컨트롤러 추가

--- src/main/java/com/orca/kim/schedule/controller/ScheduleController.java
+++ src/main/java/com/orca/kim/schedule/controller/ScheduleController.java
@@ -28,6 +28,12 @@ public class ScheduleController {
 		return "schedule/schedule";
 	}
 	
+	// scheduleClient 페이지 이동 컨트롤러
+	@RequestMapping("scheduleClient.sc")
+	public String scheduleClientPage() {
+		return "schedule/scheduleClient";
+	}
+	
 	// schedule 페이지 fullcaledar 일정 조회 ajax 컨트롤러
 	@ResponseBody
 	@RequestMapping(value="scheduleSelect.sc", produces="application/json; charset=UTF-8")
@@ -36,7 +42,6 @@ public class ScheduleController {
 	     
 	     List<Map<String, Object>> mapList = new ArrayList<Map<String, Object>>();
 	     Map<String, Object> map = new HashMap<String, Object>();
-	     System.out.println(list);
 	     
 	     for (int i = 0; i < list.size(); i++) {
 	    	 	map.put("no", list.get(i).getScheNo());
@@ -53,19 +58,41 @@ public class ScheduleController {
 		return mapList;
 	}
 	
+	// scheduleClient 페이지 fullcalendar 일정 조회 ajax 컨트롤러
+	@ResponseBody
+	@RequestMapping(value="scheduleClientSelect.sc", produces="application/json; charset=UTF-8")
+	public List<Map<String, Object>> ajaxSelectScheduleClientList(){
+		ArrayList<Schedule> list = sService.ajaxSelectScheduleClientList();
+		
+		List<Map<String, Object>> mapList = new ArrayList<Map<String, Object>>();
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		for (int i = 0; i < list.size(); i++) {
+			map.put("no", list.get(i).getScheNo());
+			map.put("start", list.get(i).getScheDateStart());
+            map.put("end", list.get(i).getScheDateEnd());
+            map.put("status", list.get(i).getScheStatus());
+            map.put("exp", list.get(i).getScheStatusExp());
+            mapList.add(map);
+            map = new HashMap<String, Object>();
+		}
+		return mapList;
+	}
+	
 	// schedule 페이지 insert 컨트롤러
 	@RequestMapping("schduleInsert.sc")
 	public String insertSchedule(Schedule s, HttpSession session) {
 		int result = sService.insertSchedule(s);
-		System.out.println(s);
+
 		if(result > 0) {
 			session.setAttribute("alertTitle", "Registration Complete");
 			session.setAttribute("alertMsg", "일정 등록 성공");
+			return "redirect:schedule.sc";
 		}else {
 			session.setAttribute("alertTitle", "Registration Fail");
 			session.setAttribute("alertMsg", "일정 등록 실패");
+			return "redirect/";
 		}
-		return "redirect:schedule.sc";
 	}
 	
 	// schedule 페이지 eventDrop ajax 컨트롤러
@@ -73,8 +100,7 @@ public class ScheduleController {
 	@RequestMapping(value="scheduleDropAjax.sc", produces="application/json; charset=UTF-8")
 	public String scheduleDropAjax(Schedule s) {
 		int result = sService.scheduleDropAjax(s);
-		System.out.println(s);
-		System.out.println(result);
+
 		if(result > 0) {
 			return new Gson().toJson("success");
 		}else {
@@ -83,18 +109,46 @@ public class ScheduleController {
 	}
 	
 	// schedule 페이지 fullcalendar detail 수정 컨트롤러
-	@RequestMapping("scheduleUpdate")
+	@RequestMapping("scheduleUpdate.sc")
 	public String updateScheduleDetail(Schedule s, HttpSession session) {
-		int result = sService.updateScheduleDetail(s);
 		
+		int result = sService.updateScheduleDetail(s);
+
 		if(result > 0) {
 			session.setAttribute("alertTitle", "Update Complete");
 			session.setAttribute("alertMsg", "수정 성공");
+			return "redirect:schedule.sc";
 		}else {
 			session.setAttribute("alertTitle", "Update Fail");
 			session.setAttribute("alertMsg", "수정 실패");
+			return "redirect/";
 		}
-		return "redirect:schedule.sc";
 	}
+	
+	
+	// schedule 페이지 delete 컨트롤러
+	@RequestMapping("scheDelete.sc")
+	public String deleteSchedule(int no, HttpSession session) {
+		
+		int result = sService.deleteSchedule(no);
+
+		if(result > 0) {
+			session.setAttribute("alertTitle", "Delete Complete");
+			session.setAttribute("alertMsg", "삭제 성공");
+			return "redirect:schedule.sc";
+		}else {
+			session.setAttribute("alertTitle", "Delete Fail");
+			session.setAttribute("alertMsg", "삭제 실패");
+			return "redirect/";
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
