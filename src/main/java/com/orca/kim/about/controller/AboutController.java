b311520e6ef00d1b3ce2f886f388285969db2222
heoseongbeom <103404620+heoseongbeom@users.noreply.github.com>
2023-05-11 15:25:37 -0700

    Update AboutController.java
    불필요한 코드 삭제

--- src/main/java/com/orca/kim/about/controller/AboutController.java
+++ src/main/java/com/orca/kim/about/controller/AboutController.java
@@ -35,7 +35,6 @@ public class AboutController {
 	public ModelAndView selectAbout(ModelAndView mv) {
 		
 		About a = aService.selectAbout();
-		System.out.println(a);
 		mv.addObject("a", a).setViewName("about/about");
 		return mv;
 	}
@@ -53,9 +52,6 @@ public class AboutController {
 	@RequestMapping("aboutUpdate.ab")
 	public String updateAbout(About a, HttpSession session,  MultipartFile upfile) {
 		
-		
-		System.out.println(upfile);
-		
 		if(!upfile.getOriginalFilename().equals("")) {
 			
 			// 기존에 첨부파일이 있었을 경우
@@ -69,62 +65,6 @@ public class AboutController {
 			a.setAboutProfileChangeImg(saveFilePath);
 		}
 		
-//		if(!file1.getOriginalFilename().equals("")) {
-//			
-//			if(a.getAboutStoryOriginImg1() != null) {
-//				new File( session.getServletContext().getRealPath(a.getAboutStoryChangeImg1()) ).delete();
-//			}
-//			
-//			String saveFilePath = FileUpload.saveFile(file1, session, "resources/uploadFiles/");
-//			a.setAboutStoryOriginImg1(file1.getOriginalFilename());
-//			a.setAboutStoryChangeImg1(saveFilePath);
-//		}
-//		
-//		if(!file2.getOriginalFilename().equals("")) {
-//			
-//			if(a.getAboutStoryOriginImg2() != null) {
-//				new File( session.getServletContext().getRealPath(a.getAboutStoryChangeImg2()) ).delete();
-//			}
-//			
-//			String saveFilePath = FileUpload.saveFile(file2, session, "resources/uploadFiles/");
-//			a.setAboutStoryOriginImg2(file2.getOriginalFilename());
-//			a.setAboutStoryChangeImg2(saveFilePath);
-//		}
-//
-//		if(!file3.getOriginalFilename().equals("")) {
-//				
-//				if(a.getAboutStoryOriginImg3() != null) {
-//					new File( session.getServletContext().getRealPath(a.getAboutStoryChangeImg3()) ).delete();
-//				}
-//				
-//				String saveFilePath = FileUpload.saveFile(file3, session, "resources/uploadFiles/");
-//				a.setAboutStoryOriginImg3(file3.getOriginalFilename());
-//				a.setAboutStoryChangeImg3(saveFilePath);
-//		}
-//			
-//		if(!file4.getOriginalFilename().equals("")) {
-//				
-//				if(a.getAboutStoryOriginImg4() != null) {
-//					new File( session.getServletContext().getRealPath(a.getAboutStoryChangeImg4()) ).delete();
-//				}
-//				
-//				String saveFilePath = FileUpload.saveFile(file4, session, "resources/uploadFiles/");
-//				a.setAboutStoryOriginImg4(file4.getOriginalFilename());
-//				a.setAboutStoryChangeImg4(saveFilePath);
-//		}
-//			
-//		if(!file5.getOriginalFilename().equals("")) {
-//				
-//				if(a.getAboutStoryOriginImg5() != null) {
-//					new File( session.getServletContext().getRealPath(a.getAboutStoryChangeImg5()) ).delete();
-//				}
-//				
-//				String saveFilePath = FileUpload.saveFile(file5, session, "resources/uploadFiles/");
-//				a.setAboutStoryOriginImg5(file5.getOriginalFilename());
-//				a.setAboutStoryChangeImg5(saveFilePath);
-//		}
-		
-		System.out.println(a);
 		int result = aService.updateAbout(a);
 		
 		if(result > 0) {
